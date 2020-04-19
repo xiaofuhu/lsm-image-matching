c06cb17b8fa80ef354453f93a6b1f5edc76a4ef6
Fuhu Xiao <fuhu@umich.edu>
2020-04-19 02:40:32 -0400

    placeholder

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
+from shift_img import shift_img
 
-IMG_A = "M.png"
-IMG_B = "M.png"
-
+IMG_A = "NEW.png"
+IMG_B = "OLD.png"
+WORK = "DONT_TOUCH_ME.png"
 IMG_SIZE = (220, 290)
+EPOCH = 10
+
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-def linearize_image(img_name):
+def init_img_size(img_name):
+    global IMG_SIZE
     img = mpimg.imread(img_name)     
     gray = rgb2gray(img)
     gray = np.array(gray)
     IMG_SIZE = np.shape(gray)
+
+def serialize_image(img_name):
+    img = mpimg.imread(img_name)     
+    gray = rgb2gray(img)
+    gray = np.array(gray)
     gray = gray.flatten()
     return gray
 
 def gen_grad(img):
+    global IMG_SIZE
     result = np.ndarray((len(img), 2))
     img = np.reshape(img, IMG_SIZE)
     for i, ei in enumerate(img):
@@ -37,14 +47,29 @@ def calc_LSM_sol(mat, dl):
     lsm = np.matmul(new_mat, dl)
     return lsm
 
-def save_result(result):
-    pass
-
-if __name__ == "__main__":
-    img_observed = linearize_image(IMG_A)
-    img_original = linearize_image(IMG_B)
+def iterate(new, old):
+    img_observed = serialize_image(new)
+    img_original = serialize_image(old)
     delta_l = img_observed - img_original
     A_matrix = gen_grad(img_original)
     result = calc_LSM_sol(A_matrix, delta_l)
-    print(result)
-    save_result(result)
+    return result
+
+def save_to_work(result):
+    mpimg.imsave(WORK, result)
+
+def load_from_work():
+    return np.array(mpimg.imread(WORK))
+
+if __name__ == "__main__":
+    init_img_size(IMG_A)
+    save_to_work(mpimg.imread(IMG_A))
+    for i in range(EPOCH):
+        result = iterate(WORK, IMG_B)
+        print((int(result[0]), int(result[1])))
+        tmp = shift_img(load_from_work(), int(result[0] * 20), int(result[1] * 20))
+        plt.imshow(tmp)
+        plt.show()
+        save_to_work(tmp)
+
+    print("finish")
--- shift_img.py
+++ shift_img.py
@@ -3,14 +3,44 @@ import cv2
 import matplotlib.pyplot as plt 
 from PIL import Image
 
-SHIFT = 50
+SHIFT_X = -5
+SHIFT_Y = 10
+
+
+def shift_img(img, x, y):
+    for i in range(len(img)):
+        if (x >= 0):
+            for j in reversed(range(len(img[i]))):
+                if j >= x:
+                    img[i][j] = img[i][j - x]
+                else:
+                    img[i][j] = img[i][0]
+        else:
+            for j in range(len(img[i])):
+                if j < len(img[i]) + x:
+                    img[i][j] = img[i][j - x]
+                else:
+                    img[i][j] = img[i][len(img[i]) - 1]
+
+    if (y >= 0):
+        for i in reversed(range(len(img))):
+            for j in range(len(img[i])):   
+                if i >= y:
+                    img[i][j] = img[i - y][j]
+                else:
+                    img[i][j] = img[0][j]
+    else:
+        for i in range(len(img)):
+            if i < len(img) + y:
+                for j in range(len(img[i])):
+                    img[i][j] = img[i - y][j]
+            else: 
+                for j in range(len(img[i])):
+                    img[i][j] = img[len(img) - 1][j]
+    return img
+
 
 if __name__ == "__main__":
-    img1 = cv2.imread('M.png')  
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    for i in img1:
-        for j, e in enumerate(reversed(i)):
-            if j > SHIFT:
-                e = img1[j + SHIFT];
-    cv2.imwrite("NEW.png", img1)
+    img = cv2.imread('OLD.png')
+    cv2.imwrite("NEW.png", shift_img(img, SHIFT_X, SHIFT_Y))
     
